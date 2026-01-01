acba5ec4d667ba5caf33a593bd3d67c1771d0621
Iyedchaabane <chaabaneiyed@gmail.com>
2026-01-01 16:43:30 +0100

    feat: add discovery service (Eureka Server)

---
+++ discovery/src/test/java/com/ichaabane/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ichaabane.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
