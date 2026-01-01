b6d7aae734998d4e2c7ac99396ae8f35dcb66703
Iyedchaabane <chaabaneiyed@gmail.com>
2026-01-01 16:49:57 +0100

    feat: add product service with Eureka and Config Server integration

---
+++ product/src/main/java/com/ichaabane/ecommerce/mapper/ProductMapper.java
@@ -0,0 +1,49 @@
+package com.ichaabane.ecommerce.mapper;
+
+import com.ichaabane.ecommerce.model.Category;
+import com.ichaabane.ecommerce.model.Product;
+import com.ichaabane.ecommerce.dto.response.ProductPurchaseResponse;
+import com.ichaabane.ecommerce.dto.request.ProductRequest;
+import com.ichaabane.ecommerce.dto.response.ProductResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductMapper {
+    public Product toProduct(ProductRequest request) {
+        return Product.builder()
+                .id(request.id())
+                .name(request.name())
+                .description(request.description())
+                .availableQuantity(request.availableQuantity())
+                .price(request.price())
+                .category(
+                        Category.builder()
+                                .id(request.categoryId())
+                                .build()
+                )
+                .build();
+    }
+
+    public ProductResponse toProductResponse(Product product) {
+        return new ProductResponse(
+                product.getId(),
+                product.getName(),
+                product.getDescription(),
+                product.getAvailableQuantity(),
+                product.getPrice(),
+                product.getCategory().getId(),
+                product.getCategory().getName(),
+                product.getCategory().getDescription()
+        );
+    }
+
+    public ProductPurchaseResponse toproductPurchaseResponse(Product product, double quantity) {
+        return new ProductPurchaseResponse(
+                product.getId(),
+                product.getName(),
+                product.getDescription(),
+                product.getPrice(),
+                quantity
+        );
+    }
+}
